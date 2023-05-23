965c0144a9f5f0e378540aa59bf4c12291612d05
Aaron Lauer <lauer.aaron@gmail.com>
2023-05-23 12:00:41 +0000

    docker test and proxy ping test

--- dot_config/private_fish/functions/proxy.fish
+++ dot_config/private_fish/functions/proxy.fish
@@ -41,9 +41,84 @@ end
 
 ## Test if we are in docker
 function is_docker
-if test (grep 'docker\|lxc' /proc/1/cgroup | wc -l) -gt 1
+if test -e /sbin/docker-init
     echo "1"
 else
     echo "0"
 end
+end
+
+## Test if the selected proxy is reachable
+function is_proxy_reachable
+  set PROXY_IS_REACHABLE 0
+  set DIG_PARAMS " " # not working in ZSH
+  # NC_PARAMS="-v -4 -w2 -z" #not working in zsh
+  #PROXY_URL=$(set +eu ; echo ${1:-"genproxy.corp.amdocs.com:8080"} ) #removed
+  #moving from genproxy to 10.x. as sometimes the DNS resolving does not work...
+
+  set ARG1 (set +eu ; echo $1"x" )
+  if [ "${ARG1}" = "x" ] ; then
+    ## Backup solution, when is_proxy_reachable is called without a param
+    if [ \( $(is_wsl) -eq "1" \) ]; then
+        export PINGHOSTIP="wslproxy.corp.amdocs.com"
+    else
+        export PINGHOSTIP="10.232.233.70" # aka "genproxy.corp.amdocs.com" ISR
+        DIG_PARAMS=" -x " # not working in zsh...
+    fi
+  fi
+
+  ###export PINGHOSTIP="${1}:8080" # not always IP, param could come also as a name to be resolved...
+
+  PROXY_URL=$(set +eu ; echo ${1:-"$PINGHOSTIP:8080"} )
+  PROXY_HOST=$(echo $PROXY_URL | cut -d":" -f1 )
+  PROXY_PORT=$(echo $PROXY_URL | cut -d":" -f2 )
+  ## test if genproxycan be reached
+  # nc is much better, as the ping is both slow 
+  # also some users don't have perms to run ping.
+
+  # if the nc binary exists, we'll use it instead of ping
+  NC_EXISTS=$( { type nc 2>/dev/null || true ; } | { grep -c " is " || true ; } )
+
+  DIG_EXISTS=$( { type dig 2>/dev/null || true ; } | { grep -c " is " || true ; } )
+
+  ## on mac, we cannot use dig <ip-address>, so our dig solution should be avoided here, and shifting to NC or PING.
+  ON_MAC=0
+  if [ \( -s /etc/bashrc_Apple_Terminal \) ]; then
+    ON_MAC=1
+  fi
+  if [ $ON_MAC -lt 1 -a $DIG_EXISTS -gt "0" ]; then
+  #if [ $DIG_EXISTS -gt "0" ]; then
+    DIG_RESOLVED=$({ dig +short +tries=1 +time=1 +search `echo $DIG_PARAMS` $PROXY_HOST 2>/dev/null || true ; } | { grep -v " timed out" || true ; } | { grep -v "no servers could be reached" || true ; } |  { grep '\.' || true ; } | { wc -l || true ; } )  # Not working in ZSH due to $DIG_PARAMS
+    #DIG_RESOLVED=$({ dig +short +tries=1 +time=1 +search $PROXY_HOST 2>/dev/null || true ; } | { grep -v " timed out" || true ; } | { grep -v "no servers could be reached" || true ; } |  { grep '\.' || true ; } | { wc -l || true ; } )
+    [ $DIG_RESOLVED -gt "0" ] && PROXY_IS_REACHABLE=1
+  elif [ $NC_EXISTS -gt "0" ]; then
+   while true; do # this should run only once; It's usefull to skip other tests if one shows there is no proxy
+    CONN_TIMEDOUT=$({ nc -v -4 -w2 -z $PROXY_HOST $PROXY_PORT 2>&1 || true ; } | { grep -c 'timed out' || true ; } )
+    [ $CONN_TIMEDOUT -eq "1" ] && PROXY_IS_REACHABLE=0 && break
+    CONN_REFUSED=$({ nc -v -4 -w2 -z $PROXY_HOST $PROXY_PORT 2>&1 || true ; } | { grep -c efused || true ; } )
+    [ $CONN_REFUSED -eq "1" ] && PROXY_IS_REACHABLE=0 && break
+    NAME_RESOLUTION_ISSUE=$({ nc -v -4 -w2 -z $PROXY_HOST $PROXY_PORT 2>&1 || true ; } | { grep -c -i name || true ; } )
+    [ $NAME_RESOLUTION_ISSUE -eq "1" ] && PROXY_IS_REACHABLE=0 && break
+    #let SOME_ERROR=CONN_TIMEDOUT+CONN_REFUSED+NAME_RESOLUTION_ISSUE
+    #if [ $SOME_ERROR -gt "0" ]; then
+    #  PROXY_IS_REACHABLE=0
+    #else
+    #  PROXY_IS_REACHABLE=1
+    #fi
+    PROXY_IS_REACHABLE=1
+    break
+   done
+  else
+    # fallback to using ping, as nc was not found. One should install nc asap, to ensure correct results.
+    P=$( { ping -4 -q -n -c 2 -W 1 -w 2 $PROXY_HOST 2>/dev/null || true ; } | { grep -c " 0% packet loss" || true ; } )
+    if [ $P -eq "1" ]; then
+      PROXY_IS_REACHABLE=1
+    else
+      PROXY_IS_REACHABLE=0
+    fi
+  fi
+
+  #return $PROXY_IS_REACHABLE
+  #we cannot return non-zero as it conflicts with set -e
+  echo "$PROXY_IS_REACHABLE"
 end
