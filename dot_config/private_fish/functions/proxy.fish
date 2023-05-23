201cc0a0d96cc163841d2be0223b38dc7983ddd1
Aaron Lauer <lauer.aaron@gmail.com>
2023-05-23 10:00:41 +0000

    fish proxy script for amdocs

--- dot_config/private_fish/functions/proxy.fish
+++ dot_config/private_fish/functions/proxy.fish
@@ -5,26 +5,45 @@
 ########
 ### CORE FUNCTIONALITY:
 function proxy
-switch "$1"
-case 'a|auto'
+switch $argv[1]
+case a auto
     proxyauto
-case 'af|auto_full|install|reinstall'
-    proxy_install_in_profile_auto && proxyauto && proxy_os_configs_auto && proxyauto && echo "check using: proxy status"
-case 'c|proxy_os_configs_auto'
+case af auto_full install reinstall 
+    proxy_install_in_profile_auto; and proxyauto; and proxy_os_configs_auto; and proxyauto; and echo "check using: proxy status"
+case c proxy_os_configs_auto
     proxy_os_configs_auto
-case 'on|ON|On'
+case on ON On
     proxyon $2
-case 'off|OFF|Off'
+case off OFF Off
     proxyoff
-case 's|status'
+case s status 
     proxystatus
-case 'install_in_profile'
+case install_in_profile
     proxy_install_in_profile
-case 'force_on|full|on_force'
-    proxyon && proxy_os_configs_on && proxy_install_in_profile_on && proxystatus
-case 'help'
+case force_on full on_force
+    proxyon; and proxy_os_configs_on; and proxy_install_in_profile_on; and proxystatus
+case help
     proxyhelp
 case '*'
     proxyon $1
+end
+end
+
+## Test if we are in WSL
+function is_wsl
+set MS_IN_UNAME (uname -r | grep -i microsoft || true )
+if test "x$WSL_DISTRO_NAME" != "x"  -o -d /run/WSL/ -o "x$MS_IN_UNAME" != "x"
+    echo "1"
+else
+    echo "0"
+end
+end
+
+## Test if we are in docker
+function is_docker
+if test (grep 'docker\|lxc' /proc/1/cgroup | wc -l) -gt 1
+    echo "1"
+else
+    echo "0"
 end
 end
